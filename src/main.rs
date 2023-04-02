9a23d562c85f62f4642c6ffd89b704daa8ac20c6
Dusan Andric <dusan.andric@gmail.com>
2023-04-01 22:24:53 -0400

    2D camera

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
-use bevy::app::App;
-use bevy::DefaultPlugins;
+use bevy::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .insert_resource(ClearColor(Color::rgb(0.20, 0.20, 0.20)))
+        .add_startup_system(setup_camera)
         .run();
+}
+
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
