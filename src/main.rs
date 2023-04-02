1a083f3ccbb4a9aa72f729ad52e4f66df1ad008f
Dusan Andric <dusan.andric@gmail.com>
2023-04-01 22:34:07 -0400

    splash screen on load

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,19 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .insert_resource(ClearColor(Color::rgb(0.20, 0.20, 0.20)))
+        .insert_resource(ClearColor(Color::hex("F5EDE9").unwrap()))
         .add_startup_system(setup_camera)
+        .add_startup_system(setup_spash)
         .run();
 }
 
 fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
+}
+
+fn setup_spash(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("branding/splash.png"),
+        ..default()
+    });
 }
