493f998e559ebb0820ae1de30ecf55804b92e494
Alex <lepsalex@gmail.com>
2023-04-04 14:30:13 -0400

    wip asset loader

--- src/assets/game_data.rs
+++ src/assets/game_data.rs
@@ -1,16 +1,50 @@
-use bevy::prelude::*;
-use bevy_common_assets::yaml::YamlAssetPlugin;
+use bevy::{utils::HashMap, prelude::{AssetServer, HandleUntyped, World, Assets}};
+use bevy_asset_loader::prelude::{DynamicAssetCollection, DynamicAssets, DynamicAsset, DynamicAssetType};
 
-pub struct GameDataPlugin;
+#[derive(serde::Deserialize, bevy::reflect::TypeUuid)]
+#[uuid = "413be529-bfeb-41b3-9db0-4b8b380a2c46"]
+pub struct GameDataAssetCollection(HashMap<String, GameDataAsset>);
 
-impl Plugin for GameDataPlugin {
-    fn build(&self, app: &mut App) {
-        app.add_plugin(YamlAssetPlugin::<GameData>::new(&["game-data.yaml"]))
-            .add_system(setup_game_data.on_startup());
+impl DynamicAssetCollection for GameDataAssetCollection {
+    fn register(&self, dynamic_assets: &mut DynamicAssets) {
+        for (key, asset) in self.0.iter() {
+            dynamic_assets.register_asset(key, Box::new(asset.clone()));
+        }
     }
 }
-#[derive(Resource)]
-pub struct GameDataHandle(pub Handle<GameData>);
+
+#[derive(serde::Deserialize, Debug, Clone)]
+enum GameDataAsset {
+    StringList(Vec<String>),
+    SymptomList(Vec<Symptom>),
+    IngredientList(Vec<Ingredient>)
+}
+
+impl DynamicAsset for GameDataAsset {
+    fn load(&self, asset_server: &AssetServer) -> Vec<HandleUntyped> {
+        match self {
+            GameDataAsset::StringList { .. } => vec![],
+            GameDataAsset::SymptomList { .. } => vec![],
+            GameDataAsset::IngredientList { .. } => vec![],
+        }
+    }
+
+    fn build(&self, world: &mut World) -> Result<DynamicAssetType, anyhow::Error> {
+        let cell = world.cell();
+        let asset_server = cell
+            .get_resource::<AssetServer>()
+            .expect("Failed to get asset server");
+
+        match self {
+            GameDataAsset::StringList { .. } => {
+                let mut stringLists = cell.get_resource_mut::<Assets<Vec<String>>>().expect("Failed to get string list asset");
+                let handle = stringLists.add(..).clone_untyped();
+
+                Ok(DynamicAssetType::Single((handle)))
+            }
+        }
+    }
+}
 
 #[derive(serde::Deserialize, Debug, Clone, bevy::reflect::TypeUuid)]
 #[uuid = "413be529-bfeb-41b3-9db0-4b8b380a2c46"]
@@ -32,9 +66,4 @@ pub struct Ingredient {
     pub name: String,
     pub cures: String,
     pub causes: Vec<String>,
-}
-
-fn setup_game_data(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let game_data = GameDataHandle(asset_server.load("data/test.game-data.yaml"));
-    commands.insert_resource(game_data);
 }
--- src/assets/mod.rs
+++ src/assets/mod.rs
@@ -1,22 +1,33 @@
 use bevy::prelude::*;
 use bevy_asset_loader::prelude::{LoadingState, LoadingStateAppExt};
+use bevy_common_assets::yaml::YamlAssetPlugin;
 
 use crate::game::state::GlobalState;
 
-use self::standard_assets::{GlobalAssets, CharacterAssets, UiAssets};
+use self::{
+    game_data::GameDataAssetCollection,
+    standard_assets::{CharacterAssets, GlobalAssets, UiAssets},
+};
 
-pub mod standard_assets;
 pub mod game_data;
+pub mod standard_assets;
 
 pub struct AssetPlugin;
 
 impl Plugin for AssetPlugin {
     fn build(&self, app: &mut App) {
-        app.add_loading_state(
+        app.add_plugin(YamlAssetPlugin::<GameDataAssetCollection>::new(&[
+            "game-data.yaml",
+        ]))
+        .add_loading_state(
             LoadingState::new(GlobalState::AssetLoading).continue_to_state(GlobalState::Splash),
         )
         .add_collection_to_loading_state::<_, GlobalAssets>(GlobalState::AssetLoading)
         .add_collection_to_loading_state::<_, CharacterAssets>(GlobalState::AssetLoading)
-        .add_collection_to_loading_state::<_, UiAssets>(GlobalState::AssetLoading);
+        .add_collection_to_loading_state::<_, UiAssets>(GlobalState::AssetLoading)
+        .add_dynamic_collection_to_loading_state::<_, GameDataAssetCollection>(
+            GlobalState::AssetLoading,
+            "data/test.game-data.yaml",
+        );
     }
 }
