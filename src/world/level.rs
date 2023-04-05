11a18dfe8361ad99a1e11481354b5c54683563bf
Alex <lepsalex@gmail.com>
2023-04-05 19:22:44 -0400

    add level bkg despawn on game state exit

--- src/world/level.rs
+++ src/world/level.rs
@@ -2,19 +2,28 @@ use bevy::prelude::*;
 
 use crate::assets::resources_standard::UiAssets;
 
-use super::global_state::GlobalState;
+use super::{despawn::despawn_entity, global_state::GlobalState};
 
 pub struct LevelPlugin;
 impl Plugin for LevelPlugin {
     fn build(&self, app: &mut App) {
-        app.add_system(level_bkg_setup.in_schedule(OnEnter(GlobalState::Game)));
+        app.add_system(level_bkg_setup.in_schedule(OnEnter(GlobalState::Game)))
+            .add_system(despawn_entity::<LevelBackground>.in_schedule(OnExit(GlobalState::Game)));
     }
 }
 
+#[derive(Reflect, Component, Default)]
+#[reflect(Component)]
+pub struct LevelBackground;
+
 fn level_bkg_setup(mut commands: Commands, ui_assets: Res<UiAssets>) {
-    commands.spawn(SpriteBundle {
-        texture: ui_assets.level_background.clone(),
-        transform: Transform::from_translation(Vec3::new(0., 0., 0.)),
-        ..default()
-    });
+    commands.spawn((
+        SpriteBundle {
+            texture: ui_assets.level_background.clone(),
+            transform: Transform::from_translation(Vec3::new(0., 0., 0.)),
+            ..default()
+        },
+        LevelBackground,
+        Name::new("Level Background Sprite"),
+    ));
 }
