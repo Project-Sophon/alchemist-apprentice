aa9753024c6f113f7b795e1fdfbfe5ea0a480629
Alex <lepsalex@gmail.com>
2023-04-06 16:54:23 -0400

    wip - need to fix how selection works with the button interactions

--- src/game/ingredients.rs
+++ src/game/ingredients.rs
@@ -1,9 +1,10 @@
-use bevy::prelude::*;
+use bevy::{asset::HandleId, prelude::*};
 
 use crate::{
-    assets::{assets_game_data::Ingredient, resources_game_data::IngredientAssets},
-    style::color::{PALETTE_CREAM, PALETTE_DARK_GOLD, PALETTE_GOLD},
+    assets::assets_game_data::Ingredient,
+    style::color::{PALETTE_CREAM, PALETTE_DARK_GOLD, PALETTE_GOLD, PALETTE_DARK_BLUE},
     ui::disable_ui::DisabledUiElement,
+    world::global_state::GlobalState,
 };
 pub struct IngredientsPlugin;
 impl Plugin for IngredientsPlugin {
@@ -11,7 +12,13 @@ impl Plugin for IngredientsPlugin {
         app.init_resource::<SelectedIngredient>()
             .register_type::<IngredientsPanel>()
             .register_type::<IngredientButton>()
-            .add_system(ingredient_button_interactions);
+            .add_systems(
+                (
+                    ingredient_button_interactions,
+                    highlight_selected_ingredient,
+                )
+                    .in_set(OnUpdate(GlobalState::Game)),
+            );
     }
 }
 
@@ -54,16 +61,52 @@ fn ingredient_button_interactions(
             Interaction::Clicked => {
                 // Update the selected ingredient (handle)
                 selected_ingredient.ingredient = Some(ingredient_button.ingredient.clone());
-
-                *color = Color::hex(PALETTE_CREAM).unwrap().into();
+                // *color = Color::hex(PALETTE_CREAM).unwrap().into();
             }
             Interaction::Hovered => {
-                *color = Color::hex(PALETTE_GOLD).unwrap().into();
+                // *color = Color::hex(PALETTE_GOLD).unwrap().into();
             }
             Interaction::None => {
-                *color = Color::hex(PALETTE_DARK_GOLD).unwrap().into();
+                // *color = Color::hex(PALETTE_DARK_GOLD).unwrap().into();
             }
         }
+    }
+}
+
+fn highlight_selected_ingredient(
+    mut ingredient_buttons: Query<(&IngredientButton, &mut BackgroundColor)>,
+    selected_ingredient: Res<SelectedIngredient>,
+) {
+    // only run when the selected ingredient has changed!
+    if !selected_ingredient.is_changed() {
+        return;
+    }
+
+    // update background color for all buttons
+    for (button, mut color) in &mut ingredient_buttons {
+        let new_color = selected_ingredient
+            .ingredient
+            .clone()
+            .map(|selected| {
+                if let HandleId::Id(id1, _) = selected.id() {
+                    info!("Selected: {}", id1);
+                }
+
+                if let HandleId::Id(id2, _) = button.ingredient.id() {
+                    info!("Button: {}", id2);
+                }
+
+                if selected.id() == button.ingredient.id() {
+                    // todo: this is working but not sticking around for some reasons
+                    return Color::hex(PALETTE_DARK_BLUE).unwrap();
+                    // return Color::hex(PALETTE_CREAM).unwrap();
+                } else {
+                    return Color::hex(PALETTE_DARK_GOLD).unwrap();
+                }
+            })
+            .unwrap_or(Color::hex(PALETTE_DARK_GOLD).unwrap());
+
+        *color = new_color.into();
     }
 }
 
