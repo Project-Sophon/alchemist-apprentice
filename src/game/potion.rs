65353bb6f40cef6f7a690af326fc7011daa7ed3f
Dusan Andric <dusan.andric@gmail.com>
2023-04-06 23:25:13 -0400

    add ingredients to potion mix

--- src/game/potion.rs
+++ src/game/potion.rs
@@ -1,14 +1,20 @@
+use core::fmt;
 use bevy::prelude::*;
 
 use crate::{
     assets::{assets_game_data::Ingredient, resources_standard::UiAssets},
     style::color::PALETTE_PURPLE,
+    world::global_state::GlobalState,
 };
+
+use super::ingredients::SelectedIngredient;
 
 pub struct PotionPlugin;
 impl Plugin for PotionPlugin {
     fn build(&self, app: &mut App) {
-        app.register_type::<PotionPanel>();
+        app.register_type::<PotionPanel>()
+            .init_resource::<PotionMix>()
+            .add_system(slot_interactions.in_set(OnUpdate(GlobalState::Game)));
     }
 }
 
@@ -16,18 +22,26 @@ impl Plugin for PotionPlugin {
 
 #[derive(Resource)]
 pub struct PotionMix {
-    pub ingredient_1: Option<Handle<Ingredient>>,
-    pub ingredient_2: Option<Handle<Ingredient>>,
-    pub ingredient_3: Option<Handle<Ingredient>>,
+    pub ingredients: [Option<Handle<Ingredient>>; 3],
     pub ready: bool,
+}
+
+impl PotionMix {
+    pub fn update_ingredients(&mut self, index: usize, ingredient: Option<Handle<Ingredient>>) {
+        self.ingredients[index] = ingredient;
+    }
+}
+
+impl fmt::Display for PotionMix {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "PotionMix: {:?}", self.ingredients)
+    }
 }
 
 impl Default for PotionMix {
     fn default() -> PotionMix {
         PotionMix {
-            ingredient_1: None,
-            ingredient_2: None,
-            ingredient_3: None,
+            ingredients: [None, None, None],
             ready: false,
         }
     }
@@ -41,7 +55,13 @@ pub struct PotionPanel;
 
 #[derive(Reflect, Component, Default)]
 #[reflect(Component)]
-pub struct PotionMixSlot;
+pub struct PotionMixSlot {
+    pub index: usize,
+}
+
+#[derive(Reflect, Component, Default)]
+#[reflect(Component)]
+pub struct FilledMixSlot;
 
 // ------ SYSTEMS ------
 
@@ -71,15 +91,15 @@ pub fn build_potion_panel(commands: &mut ChildBuilder, ui_assets: &Res<UiAssets>
                     ..default()
                 })
                 .with_children(|parent| {
-                    spawn_potion_mix_slot(parent, &ui_assets.plus_dark_gold_64);
-                    spawn_potion_mix_slot(parent, &ui_assets.plus_dark_gold_64);
-                    spawn_potion_mix_slot(parent, &ui_assets.plus_dark_gold_64);
+                    spawn_potion_mix_slot(parent, &ui_assets.plus_dark_gold_64, 0);
+                    spawn_potion_mix_slot(parent, &ui_assets.plus_dark_gold_64, 1);
+                    spawn_potion_mix_slot(parent, &ui_assets.plus_dark_gold_64, 2);
                 });
         })
         .id()
 }
 
-pub fn spawn_potion_mix_slot(commands: &mut ChildBuilder, icon: &Handle<Image>) {
+pub fn spawn_potion_mix_slot(commands: &mut ChildBuilder, icon: &Handle<Image>, index: usize) {
     commands
         .spawn((
             ButtonBundle {
@@ -91,7 +111,7 @@ pub fn spawn_potion_mix_slot(commands: &mut ChildBuilder, icon: &Handle<Image>) 
                 },
                 ..default()
             },
-            PotionMixSlot,
+            PotionMixSlot { index: index },
             Name::new("Potion Mix Slot"),
         ))
         .with_children(|parent| {
@@ -103,4 +123,44 @@ pub fn spawn_potion_mix_slot(commands: &mut ChildBuilder, icon: &Handle<Image>) 
                 Name::new("Slot Icon"),
             ));
         });
+}
+
+fn slot_interactions(
+    mut commands: Commands,
+    mut interaction_query: Query<
+        (Entity, &Interaction, &mut PotionMixSlot),
+        (Changed<Interaction>, Without<FilledMixSlot>),
+    >,
+    selected_ingredient: Res<SelectedIngredient>,
+    mut potion_mix: ResMut<PotionMix>,
+    ingredients: Res<Assets<Ingredient>>,
+) {
+    if selected_ingredient.ingredient.is_none() {
+        return;
+    }
+
+    for (entity, interaction, mut potion_mix_slot) in &mut interaction_query {
+        match *interaction {
+            Interaction::Clicked => {
+                commands.entity(entity).despawn_descendants();
+                commands.entity(entity).with_children(|parent| {
+                    let handle = selected_ingredient.ingredient.clone().unwrap();
+                    let ingredient = ingredients.get(&handle).unwrap();
+                    potion_mix
+                        .update_ingredients(potion_mix_slot.index, Option::Some(handle.clone()));
+                    parent.spawn((
+                        ImageBundle {
+                            image: UiImage::new(ingredient.texture.clone()),
+                            ..default()
+                        },
+                        Name::new(format!("Selected: {}", ingredient.name)),
+                    ));
+
+                    info!("{}", potion_mix.to_string());
+                });
+            }
+            Interaction::Hovered => {}
+            Interaction::None => {}
+        }
+    }
 }
