9af50c198aa02c9378f7abc1b2f022bffdedd74d
Dusan Andric <dusan.andric@gmail.com>
2023-04-08 01:08:04 -0400

    simple select

--- src/game/bjorn.rs
+++ src/game/bjorn.rs
@@ -1,7 +1,5 @@
-use anyhow::Ok;
 use bevy::prelude::*;
 use std::collections::HashSet;
-use std::time::{SystemTime, UNIX_EPOCH};
 
 use crate::{
     assets::{assets_game_data::Symptom, resources_game_data::SymptomAssets},
@@ -39,23 +37,14 @@ fn setup_initial_bjorn_status(
     mut bjorn_status: ResMut<BjornStatus>,
     symptom_assets: Res<Assets<Symptom>>,
 ) {
+    let then = Instant::now();
     let mut initial_symptom_pool: HashSet<Symptom> = HashSet::new();
     info!("Setup initial status on entering game state...");
     for (_, symptom) in symptom_assets.iter() {
         initial_symptom_pool.insert(symptom.clone());
     }
 
-    let n = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
-    let digits: Vec<_> = n
-        .as_secs()
-        .to_string()
-        .chars()
-        .map(|d| d.to_digit(10).unwrap())
-        .collect();
-    let digit = digits.last().unwrap_or(&0);
-    let index = usize::try_from(digit % 2).unwrap();
-
-    let initial_symptom = Vec::from_iter(&initial_symptom_pool)[index];
+    let initial_symptom = Vec::from_iter(&initial_symptom_pool)[0];
     bjorn_status.symptoms = HashSet::from_iter(vec![initial_symptom.clone()]);
     info!("Initial Symptoms of Bjorn: {:?}", bjorn_status.symptoms);
 }
